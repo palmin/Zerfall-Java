6934f9db82c8c3c20488f57b79915232279193d1
Campbell Jones <gingerdeadshot@gmail.com>
2015-12-08 10:21:18 -0500

    Update Image.java

--- src/com/exosoft/zerfall/Image.java
+++ src/com/exosoft/zerfall/Image.java
@@ -9,16 +9,32 @@ public class Image extends BufferedImage {
 	private BufferedImage image;
 	private int x;
 	private int y;
+	private int translateX;
+	private int translateY;
+	private boolean translate;
 	public Image(String filename) {
 		image = ImageIO.read(new File(filename));
 	}
 	
 	public void draw(int x, int y) {
+		translate = false;
+		repaint();
+	}
+	
+	public void draw(int x, int y, int translateX, int translateY) {
+		translate = true;
 		repaint();
 	}
 	
 	public void paint(Graphics g) {
+        if (translate == true) {
+        	g.translate(translateX, translateY);
+        }
         g.drawImage(image, x, y, null);
     }
+
+	public int get(int x, int y) {
+		Color.rgb(image.getRGB());
+	}
 
 }

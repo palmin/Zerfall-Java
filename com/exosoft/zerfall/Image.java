ff0ae589a91fb43a244a73775320f3416a445d9e
Campbell Jones <gingerdeadshot@gmail.com>
2015-12-09 20:56:44 -0500

    Misc changes

--- com/exosoft/zerfall/Image.java
+++ com/exosoft/zerfall/Image.java
@@ -9,54 +9,58 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 @SuppressWarnings("serial")
-public class Image extends Applet {
+public class Image extends Main {
 	private BufferedImage image;
 	private int x;
 	private int y;
 	private int translateX;
 	private int translateY;
 	private boolean translate;
+
 	public Image() {
 		image = null;
 	}
-	
+
 	public Image(String filename) throws IOException {
-		image = ImageIO.read(new File("/resources/" + filename));
+		System.out.println("Reading image file " + filename);
+		image = ImageIO.read(new File("resources/" + filename));
 	}
-	
+
 	public Image(BufferedImage img) {
 		image = img;
 	}
-	
+
 	public void draw(int x, int y) {
 		translate = false;
-		repaint();
 	}
-	
+
 	public void draw(int x, int y, int translateX, int translateY) {
 		translate = true;
-		repaint();
 	}
-	
+
 	public void paint(Graphics g) {
-        if (translate == true) {
-        	g.translate(translateX, translateY);
-        }
-        g.drawImage(image, x, y, null);
-    }
+		if (translate == true) {
+			g.translate(translateX, translateY);
+		}
+		g.drawImage(image, x, y, null);
+	}
 
 	public int get(int x, int y) {
-		return image.getRGB(x, y);
+		if (x >= image.getWidth() || x < 0 || y >= image.getHeight() || y < 0) {
+			return 0x00000000;
+		} else {
+			return image.getRGB(x, y);
+		}
 	}
-	
+
 	public Image get(int x, int y, int w, int h) {
 		return new Image(image.getSubimage(x, y, w, h));
 	}
-	
+
 	public int width() {
 		return image.getWidth();
 	}
-	
+
 	public int height() {
 		return image.getHeight();
 	}
--- com/exosoft/zerfall/Main.java
+++ com/exosoft/zerfall/Main.java
@@ -1,48 +1,71 @@
 package com.exosoft.zerfall;
 
-import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.ArrayList;
-
+H
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-@SuppressWarnings("serial")
-public abstract class Main extends Applet {
+public class Main {
 	static Image bitmap, map, foreground;
 	static boolean keys[];
-	static boolean itemsLoaded;
+	static boolean itemsLoaded = true;
 	static Font orbitron = new Font("fonts/orbitron.ttf", Font.PLAIN, 36);
 	static ArrayList<zombieClass> zombies;
-	static Player Player;
-	//XML guns;
+	static Player player;
+	// XML guns;
 	public static JFrame window;
 	public static JLabel label;
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException, UnsupportedAudioFileException, LineUnavailableException {
 		window = new JFrame("Zerfall");
 		window.setSize(1280, 720);
-		window.setVisible(true);
+		window.setVisible(true);S
 		label = new JLabel("First Name");
 		label.setForeground(Color.GRAY);
 		zombies = new ArrayList<zombieClass>();
-		try {
-			loadItems();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		while (itemsLoaded == false) {
-		}
+		bitmap = new Image("Maps/bitmap.png");
+		map = new Image("Maps/map.png");
+		foreground = new Image("Maps/foreground.png");
+		keys = new boolean[128];
+		player = new Player();
+		// guns = loadXML("Resources/Gun Information.xml");
 		while (itemsLoaded) {
-			Player.weapon();
-			Player.movement();
+			player.weapon();
+			player.movement();
 			for (zombieClass zombie : zombies)
 				zombie.movement();
 		}
+	}
+
+	public static void paint(Graphics g) {
+		int translateX = (int) (-(player.xpos + player.sheet[0].width() / 2) + window.getWidth() / 2);
+		int translateY = (int) (-(player.ypos + player.sheet[0].height() / 2) + window.getHeight() / 2);
+		map.draw(0, 0, translateX, translateY);
+		player.sheet[player.sprite].draw(player.xpos, player.ypos, translateX, translateY);
+		for (zombieClass zombie : zombies)
+			zombie.sheet.get(zombie.sprite * 100, 0, 100, 162).draw(zombie.xpos, zombie.ypos, translateX, translateY);
+		foreground.draw(0, 0, translateX, translateY);
+		printText(Integer.toString(player.gunClip) + "/" + Integer.toString(player.clipSize[player.weapon]), 1270, 700,
+				36, 0xFFFFFF, JLabel.RIGHT);
+		printText(player.gunID[player.weapon], 1270, 660, 24, 0xEEEEEE, JLabel.RIGHT);
+	}
+
+	static void printText(String text, int x, int y, int size, int fill, int align) {
+		JLabel label = new JLabel(text, align);
+		label.setFont(orbitron);
+		label.setForeground(new Color(fill));
+		label.setLocation(x, y);
+	}
+
+	void printText(int text, int x, int y, int size, int fill, int align) {
+		printText(Integer.toString(text), x, y, size, fill, align);
 	}
 
 	public void keyPressed(KeyEvent evt) {
@@ -53,92 +76,242 @@ public abstract class Main extends Applet {
 		keys[evt.getKeyCode()] = false;
 	}
 
-	public static synchronized void loadItems() throws IOException { 
-			bitmap = new Image("Maps/bitmap.png");
-			map = new Image("Maps/map.png");
-			foreground = new Image("Maps/foreground.png");
-			keys = new boolean[128];
-			//guns = loadXML("Resources/Gun Information.xml");
-			itemsLoaded = true;
+	public static class Player extends Main {
+		Image sheet[] = new Image[8];
+		int sprite = 0;
+		int xpos = 2270;
+		int ypos = 940;
+		int yspeed = 1;
+		int weapon = 0;
+		int gunClip = 30;
+		int gunRPM[] = { 6, 5, 120, 48, 3, 6, 600, 144, 6, 5, 6, 90, 7, 240, 6, 4, 6 };
+		int clipSize[] = { 30, 30, 10, 30, 25, 20, 5, 7, 20, 50, 100, 10, 32, 8, 75, 150, 32 };
+		int boltPosition = 0;
+		boolean collision[] = new boolean[5];
+		Audio gunAudio[][] = new Audio[2][18], dryfire;
+		timer swap = new timer(.2), reload = new timer(1);
+		String gunID[] = { "AK47", "AUG", "Dragunov", "FAL", "FAMAS", "G3", "L2A3", "M1911", "M1918", "M1928", "M60",
+				"M9", "MP40", "PPK", "RPK", "Stoner63", "Uzi" };
+
+		public Player() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+			for (int i = 0; i < gunID.length; i++) {
+				gunAudio[0][i] = new Audio("Sounds/Guns/" + gunID[i] + " Gunshot.ogg");
+				gunAudio[1][i] = new Audio("Sounds/Guns/" + gunID[i] + " Reload.ogg");
+			}
+			Image temp = new Image("sprites/player.png");
+			for (int i = 0; i < 8; i++)
+				sheet[i] = temp.get(i * 175, 0, 175, 161);
+			temp = null;
+			dryfire = new Audio("Sounds/Dry-Fire.ogg");
 		}
 
-}
+		void movement() {
+			for (int i = 0; i < 5; i++)
+				collision[i] = false;
+			for (int x = xpos + 25; x <= xpos + 150; x++) {
+				for (int y = ypos + 161; y <= ypos + 162 + Math.abs(yspeed); y++) {
+					int c = bitmap.get(x, y);
+					switch (c) {
+					case 0x000000:
+						collision[1] = true;
+						break;
+					case 0x0000FF:
+						collision[0] = true;
+						break;
+					}
+				}
+			}
+			for (int x = xpos + 25; x <= xpos + 150; x++) {
+				for (int y = ypos; y <= ypos - 1 - Math.abs(yspeed); y--) {
+					int c = bitmap.get(x, y);
+					switch (c) {
+					case 0x000000:
+						collision[2] = true;
+						break;
+					}
+				}
+			}
+			for (int x = xpos + 20; x <= xpos + 25; x++) {
+				for (int y = ypos; y <= ypos + 161; y++) {
+					int c = bitmap.get(x, y);
+					switch (c) {
+					case 0xFF0000:
+						if (keys[69])
+							doors(x, y);
+					case 0x000000:
+						collision[3] = true;
+						break;
+					}
+				}
+			}
+			for (int x = xpos + 150; x <= xpos + 155; x++) {
+				for (int y = ypos; y <= ypos + 161; y++) {
+					int c = bitmap.get(x, y);
+					if (collision[4] == false && (c == 0xFF0000 || c == 0x000000))
+						collision[4] = true; // Right bound
+					if (c == 0xFF0000 && keys[69])
+						doors(x, y);
+				}
+			}
+			if (keys[65] && collision[3] == false)
+				xpos -= 5;
+			if (keys[68] && collision[4] == false)
+				xpos += 5;
+			if (keys[65] && collision[1] && keys[' '] == false) {
+				sprite = 0;
+			} else if (keys[68] && collision[1] && keys[' '] == false) {
+				sprite = 2;
+			}
+			if (keys[87] && collision[2] == false) {
+				if (collision[1]) {
+					yspeed = -10;
+					collision[1] = false;
+					if (sprite % 2 == 0)
+						sprite++;
+				}
+				if (collision[0]) {
+					yspeed = -4;
+					collision[1] = false;
+				}
+			}
+			yspeed = (collision[1] || collision[2]) ? 1 : yspeed + 1;
+			ypos += (collision[1] == false) ? yspeed : 0;
+			if (sprite > 3 && boltPosition != 2) {
+				sprite -= 4;
+			} else if (keys[32] && gunClip > 0 && sprite <= 3) {
+				sprite += 4;
+			}
+		}
 
-@SuppressWarnings("serial")
-class zombieClass extends Main {
-	Image sheet = null;
-	int sprite = 0, xpos = 2270, ypos = 940, health = 100, yspeed = 1, xspeed;
-	boolean collision[] = new boolean[5];
-	zombieClass(int xspeed) throws IOException {
-		sheet = new Image("sprites/zombie.png");
+		public void doors(int x, int y) {
+			int doorColor = 0xFF0000;
+			int w = 0;
+			int h = 0;
+			while (bitmap.get(x - 1, y) == doorColor)
+				x--;
+			while (bitmap.get(x, y - 1) == doorColor)
+				y--;
+			while (bitmap.get(x + w + 1, y) == doorColor)
+				w++;
+			while (bitmap.get(x, y + h + 1) == doorColor)
+				h++;
+			for (int x2 = x; x2 < x + w + 1; x2++) {
+				for (int y2 = y; y2 < y + h + 1; y2++) {
+					bitmap.set(x2, y2, 0xFFFFFF);
+				}
+			}
+		}
+
+		public void weapon() {
+			if (reload.active) {
+				reload.check();
+				if (reload.active == false)
+					gunClip = clipSize[weapon];
+			}
+			if (swap.active)
+				swap.check();
+
+			if (keys[70] && swap.active == false) {
+				weapon = (weapon < 16) ? weapon + 1 : 0;
+				gunClip = clipSize[weapon];
+				swap.activate();
+				reload.set(gunAudio[1][weapon].length());
+			}
+			if (keys[32] && reload.active == false) {
+				if (boltPosition == 1 && gunClip > 0) {
+					gunAudio[0][weapon].play();
+					gunClip--;
+				} else if (boltPosition == 1 && gunClip == 0) {
+					dryfire.play();
+				}
+			}
+			boltPosition = (keys[32] && boltPosition <= gunRPM[weapon]) ? boltPosition + 1 : 1;
+			if (keys[82] && reload.active == false) { // If the R key is pressed
+				reload.activate();
+				gunAudio[1][weapon].play();
+			}
+		}
+
 	}
 
-	void movement() {
-		for (int i = 0; i < 5; i++)
-			collision[i] = false;
-		for (int x = xpos; x <= xpos + 100; x++) {
-			for (int y = ypos + 161; y <= ypos + 161 + Math.abs(yspeed); y++) {
-				int c = bitmap.get(x, y);
-				collision[1] = (c == 0xFF0000 || c == 0x0000FF) ? true : collision[1];
-				collision[0] = (c == 0x0000FF) ? true : collision[0];
+	class zombieClass extends Main {
+		Image sheet = null;
+		int sprite = 0, xpos = 2270, ypos = 940, health = 100, yspeed = 1, xspeed;
+		boolean collision[] = new boolean[5];
+
+		zombieClass(int xspeed) throws IOException {
+			sheet = new Image("sprites/zombie.png");
+		}
+
+		void movement() {
+			for (int i = 0; i < 5; i++)
+				collision[i] = false;
+			for (int x = xpos; x <= xpos + 100; x++) {
+				for (int y = ypos + 161; y <= ypos + 161 + Math.abs(yspeed); y++) {
+					int c = bitmap.get(x, y);
+					collision[1] = (c == 0xFF0000 || c == 0x0000FF) ? true : collision[1];
+					collision[0] = (c == 0x0000FF) ? true : collision[0];
+				}
+			}
+			for (int x = xpos; x <= xpos + 100; x++) {
+				for (int y = ypos; y <= ypos - 1 - Math.abs(yspeed); y--) {
+					int c = bitmap.get(x, y);
+					collision[2] = (c == 0x0000FF) ? true : collision[2];
+				}
+			}
+			for (int x = xpos; x <= xpos - xspeed; x--) {
+				for (int y = ypos; y <= ypos + 162; y++) {
+					int c = bitmap.get(x, y);
+					collision[3] = (c == 0xFF0000 || c == 0x0000FF) ? true : collision[3];
+				}
+			}
+			for (int x = xpos + 100; x <= xpos + 100 + xspeed; x++) {
+				for (int y = ypos; y <= ypos + 162; y++) {
+					int c = bitmap.get(x, y);
+					collision[4] = (c == 0x0000FF || c == 0xFF0000) ? true : collision[4];
+				}
+			}
+			if (collision[1] || collision[2]) {
+				yspeed = 1;
+			} else {
+				yspeed++;
+			}
+			if (collision[0] && player.ypos < ypos && collision[2] == false) {
+				yspeed = -xspeed;
+				collision[1] = false;
+			}
+			if (collision[1] == false)
+				ypos += yspeed;
+			if (player.xpos > xpos && player.ypos > ypos - 40 && player.ypos < ypos + 40) {
+				xpos = xpos + xspeed;
+				sprite = 1;
+			} else if (player.xpos + 50 < xpos && player.ypos > ypos - 5 && player.ypos < ypos + 5) {
+				xpos = xpos - xspeed;
+				sprite = 0;
 			}
 		}
-		for (int x = xpos; x <= xpos + 100; x++) {
-			for (int y = ypos; y <= ypos - 1 - Math.abs(yspeed); y--) {
-				int c = bitmap.get(x, y);
-				collision[2] = (c == 0x0000FF) ? true : collision[2];
-			}
-		}
-		for (int x = xpos; x <= xpos - xspeed; x--) {
-			for (int y = ypos; y <= ypos + 162; y++) {
-				int c = bitmap.get(x, y);
-				collision[3] = (c == 0xFF0000 || c == 0x0000FF) ? true : collision[3];
-			}
-		}
-		for (int x = xpos + 100; x <= xpos + 100 + xspeed; x++) {
-			for (int y = ypos; y <= ypos + 162; y++) {
-				int c = bitmap.get(x, y);
-				collision[4] = (c == 0x0000FF || c == 0xFF0000) ? true : collision[4];
-			}
-		}
-		if (collision[1] || collision[2]) {
-			yspeed = 1;
-		} else {
-			yspeed++;
-		}
-		if (collision[0] && Player.ypos < ypos && collision[2] == false) {
-			yspeed = -xspeed;
-			collision[1] = false;
-		}
-		if (collision[1] == false)
-			ypos += yspeed;
-		if (Player.xpos > xpos && Player.ypos > ypos - 40 && Player.ypos < ypos + 40) {
-			xpos = xpos + xspeed;
-			sprite = 1;
-		} else if (Player.xpos + 50 < xpos && Player.ypos > ypos - 5 && Player.ypos < ypos + 5) {
-			xpos = xpos - xspeed;
-			sprite = 0;
-		}
+
 	}
-}
-class timer extends Main {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	double activation, duration;
-	boolean active;
-	timer(double duration) {
-	}
-	void check() {
-		if (System.currentTimeMillis() * .001 - activation > duration)
-			active = false;
-	}
-	void activate() {
-		activation = System.currentTimeMillis() * .001;
-		active = true;
-	}
-	void set(float d) {
-		duration = d;
+
+	class timer extends Main {
+		double activation, duration;
+		boolean active;
+
+		timer(double duration) {
+		}
+
+		void check() {
+			if (System.currentTimeMillis() * .001 - activation > duration)
+				active = false;
+		}
+
+		void activate() {
+			activation = System.currentTimeMillis() * .001;
+			active = true;
+		}
+
+		void set(float d) {
+			duration = d;
+		}
 	}
 }
